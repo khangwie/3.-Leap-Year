fda20331ef04950232037445f6c348ddce6ed6f3
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-16 12:03:03 +0200

    Add project files.

---
+++ 3. Leap Year/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Leap_Year
+{
+    internal class Program
+    {
+        static bool IfYearIsLeap(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine(" Check Leap year");
+            // check if a year leap year
+            int year = 2816;
+            int year2 = 2018;
+
+            bool IsLeapYear = IfYearIsLeap(year);
+            bool isLeapYear2 = IfYearIsLeap(year2);
+            Console.WriteLine($"IsleapYear ({year}) -> {IsLeapYear}");
+            Console.WriteLine($"IsleapYear ({year}) -> {isLeapYear2}");
+            Console.ReadLine();
+        }
+    }
+}
